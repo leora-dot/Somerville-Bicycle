414216e63985588572684e871f9d2c30f18cd0a4
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-06-25 22:42:20 -0400

    converted dates to_datetime for both crash & police

--- script.py
+++ script.py
@@ -11,18 +11,18 @@ police_bike_df = police_df[ (police_df.inctype == "BIKEVIOL") | (police_df.incty
 #Create date column
 police_bike_df['str_split'] = police_bike_df.dtreceived.str.split(" ")
 police_bike_df["date"] = police_bike_df.str_split.str.get(0)
+police_bike_df["date"] = pd.to_datetime(police_bike_df.date)
 police_bike_df.drop(columns = ["str_split"], inplace = True)
 
 police_bike_df.drop(columns = ["incnum", "inctypecode", "dtreceived", "stnum", "stname2"], inplace = True)
 
-print(police_bike_df.head())
 #print(police_bike_df.dtypes)
 
 #CLEANING CRASH DATA
 
 crash_bike_df = crash_df[crash_df.Bicycle == 1].reset_index(drop = True)
 
-crash_bike_df.drop(columns = ['City', 'State', 'Weather (2&3)',
+crash_bike_df.drop(columns = ['City', 'Time', 'State', 'Weather (2&3)',
        'Road Surface (6)', 'Traffic Control Device (4)',
        'Intersection Type (7)', 'Traffic Way Desc (8)', '# of Involved Vehicles',
        'Vehicle1 Configuration (21)', 'Driver1 Contrib Code1 (25)',
@@ -47,6 +47,9 @@ crash_bike_df.drop(columns = ['City', 'State', 'Weather (2&3)',
        'column 75', 'column 76', 'column 77', 'column 78', 'column 79',
        'column 80', 'column 81', 'column 82', 'column 83', 'column 84',
        'column 85', 'column 86', 'column 87', 'column 88', 'column 89', 'Collision Manner (11)', 'Manner of Non-Motorist (Person) Collision'], inplace = True)
+
+#formating date column
+crash_bike_df["Date"] = pd.to_datetime(crash_bike_df.Date)
 
 print(crash_bike_df.head())
 #print(crash_bike_df.columns)
