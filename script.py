0d5d593dd077ab57330735d28b9900efe6842d09
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-06-25 22:00:01 -0400

    Created bicycle specific dataframes for police & crash

--- script.py
+++ script.py
@@ -2,3 +2,21 @@ import pandas as pd
 
 police_df = pd.read_csv("Police_Traffic_Enforcement_Activity.csv")
 crash_df = pd.read_csv("Motor_Vehicle_Crash_Reports.csv")
+
+#Filtering Police DF to relevant pieces
+
+#print(police_df.columns)
+#print(police_df.inctype.value_counts())
+
+police_bike_df = police_df[ (police_df.inctype == "BIKEVIOL") | (police_df.inctype == "BIKE STOP") ].reset_index(drop = True)
+
+#print(police_bike_df.head(10))
+
+#Filtering Crash DF to relevant pieces
+
+#print(crash_df.columns)
+#print(crash_df.Bicycle.value_counts())
+
+crash_bike_df = crash_df[crash_df.Bicycle == 1].reset_index(drop = True)
+
+print(crash_bike_df.head())
